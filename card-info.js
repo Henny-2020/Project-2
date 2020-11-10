41b096111cbd13b4ed62072f4763c121a79fd9e4
Glitch (henny-project2) <none>
2020-11-10 21:39:43 +0000

    🎐☺️ Checkpoint ./styles.css:21938626/2146 ./card-info.js:21938626/435

--- card-info.js
+++ card-info.js
@@ -1,5 +1,7 @@
 $(".js-show-content").click(function() {
-  $(this).closest(".js-card-hover").addClass("is-displaying-content");
+  $(this)
+    .closest(".js-card-hover")
+    .addClass("is-displaying-content");
 });
 
 $(".js-card-hover").mouseleave(function() {
